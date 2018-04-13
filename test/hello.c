65f946a47ba6f6be4f06ece097446e47b449a0e0
Nisarg Dabhi <nisargdbh@gmail.com>
2018-04-13 19:46:25 -0400

    Passing Process ID

--- test/hello.c
+++ test/hello.c
@@ -6,9 +6,20 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Lakshmanan");
 MODULE_DESCRIPTION("A Simple Hello World module");
 
+
+//Global Variables for insmod: just need PID
+static int   pid = -1;
+//obtain PID from outer program
+module_param(pid, int, 0);
+
+
+
 static int __init hello_init(void)
 {
   printk(KERN_INFO "Hello world!\n");
+
+  printk(KERN_ALERT "Hello process ID is: %i!\n", pid);
+  
   return 0;    // Non-zero return means that the module couldn't be loaded.
 }
 
--- test/runhello.c
+++ test/runhello.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(void) {
+  //get process ID
+  int PID;
+  PID = getpid();
+  printf("Hello Process PID from getpid() is:%d!\n", PID);
+  //add number to num
+  char num[20];
+  sprintf(num, "%d", PID);
+  printf("The PID after sprintf is:%s\n", num);
+  char str[100];
+  strcpy(str, "sudo insmod hello.ko");
+  strcat(str, " pid=");
+  strcat(str, num);
+  printf("My string is:%s\n", str);
   //attempt to load kernel module using insmod
   system("sudo insmod hello.ko");
 
